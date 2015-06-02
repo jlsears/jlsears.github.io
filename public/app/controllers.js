8c53aa81f491330586cf959f8a2616f6aed4ddd0
jlsears <jen.lauren.sears@gmail.com>
2015-06-01 22:52:51 -0500

    contact page updated

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -9,9 +9,9 @@ controllers.controller('AboutController', ['$scope', function($scope){
   $scope.message = 'Hello, I am the About page, folks!';
   $scope.aboutTitle = "About Jen";
   $scope.aboutContent = "In short, English major gone tech. Native Floridian who has abdicated and returned to her roots in the South.";
-  $scope.secondContent = "She is notorious for both her ways with the written and spoken word, as well as marvelous feats of detail-orientation. Along with some fairly cheeky endeavours in cross-stitching from time to time.";
+  $scope.secondContent = "She is notorious for her ways with both the written and spoken word, as well as marvelous feats of detail-orientation, and artfully bridging the tech-to-non-tech communication gap. Along with some fairly cheeky endeavours in custom cross-stitching from time to time.";
   $scope.thirdContent = "Having reached a juncture in life where... something something... Nashville Software School... two sides of brain joining forces... something something here.";
-
+  $scope.quote = "'Make voyages. Attempt them. There's nothing else.' Tennessee Williams";
 }]);
 
 controllers.controller('ExperienceController', ['$scope', function($scope){
@@ -19,7 +19,8 @@ controllers.controller('ExperienceController', ['$scope', function($scope){
 }]);
 
 controllers.controller('ContactController', ['$scope', function($scope){
-  $scope.message = 'Contact page here!';
+  $scope.contactTitle = 'Contact';
+  $scope.email = 'jen.lauren.sears@gmail.com';
 }]);
 
 controllers.controller('SamplesController', ['$scope', function($scope){
