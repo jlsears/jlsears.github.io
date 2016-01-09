df6e2cc821efe932027e1d162a7ed55596d07c7f
jlsears <jen.lauren.sears@gmail.com>
2016-01-09 14:47:11 -0600

    revamp

--- public/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  'use strict';
-
-  angular.module('demo', [])
-  .controller('MainController', ['$scope', function($scope){
-    $scope.description="Portfolio App";
-  }])
-})();
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-/*GET home page. */
-exports.index = function(req, res) {
-  res.render('index', { title: 'used to be a title here'});
-};
-
-exports.partials = function(req, res) {
-  res.render('partials/' + req.params.name);
-};
